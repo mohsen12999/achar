89f59e9d927c574e1ae7db73e1f06d1452ba060a
mohsen12999 <mohsen1299@gmail.com>
2019-08-13 22:54:02 +0430

    add new page and use material component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import { Switch, Route } from "react-router";
+import Main from "./Main";
+import NotFound from "./NotFound";
+import About from "./About";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>app name</h3>
       </header>
+      <nav>
+        <Navbar />
+      </nav>
+      <section>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </section>
+      <footer>
+        <p>footer</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
